bbaf0bf623de54071102018d1724d07c18febae4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-14 17:57:21 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,87 +1,84 @@
 import React from 'react';
-import { Formik } from 'formik';
-import * as yup from 'yup';
 import { nanoid } from 'nanoid';
 import { BsFillTelephoneFill, BsPersonFill } from 'react-icons/bs';
 import { IoMdPersonAdd } from 'react-icons/io';
 import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
 
-import {
-  Form,
-  FormField,
-  FieldFormik,
-  ErrorMessage,
-  StyledButton,
-  LabelWrapper,
-} from './ContactForm.styled';
-
-const schema = yup.object().shape({
-  name: yup
-    .string()
-    .trim()
-    .matches(
-      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-      'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d`Artagnan'
-    )
-    .required(),
-  number: yup
-    .string()
-    .trim()
-    .matches(
-      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
-      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-    )
-    .required(),
-});
-const initialValues = { name: '', number: '' };
+import { StyledButton, LabelWrapper } from './ContactForm.styled';
 
 export const ContactForm = ({ onAddContact }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm({
+    mode: 'onBlur',
+  });
+
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values, { resetForm }) => {
-        //console.log(values);
-        //console.log(actions); // resetForm, validateForm, ...
-        onAddContact({ id: nanoid(), ...values });
-        //console.log(values);
-        resetForm();
-      }}
-      validationSchema={schema}
+    <form
+      autoComplete="off"
+      //initialValues={initialValues}
+      onSubmit={handleSubmit(data => {
+        onAddContact({ id: nanoid(), ...data });
+        reset();
+      })}
     >
-      <Form autoComplete="off">
-        <FormField htmlFor="name">
-          <LabelWrapper>
-            <BsPersonFill />
-            Name
-          </LabelWrapper>
-          <FieldFormik
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <ErrorMessage name="name" component="span" />
-        </FormField>
-        <FormField htmlFor="number">
-          <LabelWrapper>
-            <BsFillTelephoneFill /> Number
-          </LabelWrapper>
-          <FieldFormik
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-          <ErrorMessage name="number" component="span" />
-        </FormField>
-        <StyledButton type="submit">
-          <IoMdPersonAdd size="16" />
-          Add contact
-        </StyledButton>
-      </Form>
-    </Formik>
+      <label htmlFor="name">
+        <LabelWrapper>
+          <BsPersonFill />
+          Name
+        </LabelWrapper>
+        <input
+          {...register('name', {
+            required: `This field is required`,
+            pattern:
+              /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+          })}
+        />
+        <div>
+          {errors?.name && (
+            <p>
+              {errors?.name?.message ||
+                `Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore
+            d'Artagnan`}
+            </p>
+          )}
+        </div>
+      </label>
+
+      <label htmlFor="number">
+        <LabelWrapper>
+          <BsFillTelephoneFill /> Number
+        </LabelWrapper>
+        <input
+          type="tel"
+          {...register('number', {
+            required: `This field is required`,
+            min: {
+              value: 7,
+              message: `Min 7 numbers. Phone number must be digits and can contain spaces, dashes, parentheses and can start with +`,
+            },
+            pattern:
+              /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+          })}
+        />
+        <div>
+          {errors?.number && (
+            <p>
+              {errors?.number?.message ||
+                `Phone number must be digits and can contain spaces, dashes, parentheses and can start with +`}
+            </p>
+          )}
+        </div>
+      </label>
+      <StyledButton type="submit" disabled={!isValid}>
+        <IoMdPersonAdd size="16" />
+        Add contact
+      </StyledButton>
+    </form>
   );
 };
 
