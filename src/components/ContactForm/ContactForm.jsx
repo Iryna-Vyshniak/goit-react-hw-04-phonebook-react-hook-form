02f01be6b040ecf5ccf82ab6bf2f0181a5a1405e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-14 18:44:43 +0300

    add setFocus, placeholder and rules for  minLength - value and message

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,59 +5,71 @@ import { IoMdPersonAdd } from 'react-icons/io';
 import PropTypes from 'prop-types';
 import { useForm } from 'react-hook-form';
 
-import { StyledButton, LabelWrapper } from './ContactForm.styled';
+import {
+  StyledButton,
+  LabelWrapper,
+  ErrorMessage,
+  Form,
+  FormField,
+  FieldInput,
+} from './ContactForm.styled';
 
 export const ContactForm = ({ onAddContact }) => {
   const {
     register,
     handleSubmit,
+    setFocus,
     formState: { errors, isValid },
     reset,
   } = useForm({
     mode: 'onBlur',
   });
 
+  React.useEffect(() => {
+    setFocus('name');
+  }, [setFocus]);
+
   return (
-    <form
+    <Form
       autoComplete="off"
-      //initialValues={initialValues}
       onSubmit={handleSubmit(data => {
         onAddContact({ id: nanoid(), ...data });
         reset();
       })}
     >
-      <label htmlFor="name">
+      <FormField htmlFor="name">
         <LabelWrapper>
           <BsPersonFill />
           Name
         </LabelWrapper>
-        <input
+        <FieldInput
+          type="text"
+          placeholder="Your name"
           {...register('name', {
             required: `This field is required`,
             pattern:
               /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
           })}
         />
-        <div>
-          {errors?.name && (
-            <p>
-              {errors?.name?.message ||
-                `Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore
+        {errors?.name && (
+          <ErrorMessage>
+            {errors?.name?.message ||
+              `Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore
             d'Artagnan`}
-            </p>
-          )}
-        </div>
-      </label>
+          </ErrorMessage>
+        )}
+      </FormField>
 
-      <label htmlFor="number">
+      <FormField htmlFor="number">
         <LabelWrapper>
           <BsFillTelephoneFill /> Number
         </LabelWrapper>
-        <input
+        <FieldInput
           type="tel"
+          placeholder="+123-45-67"
           {...register('number', {
             required: `This field is required`,
-            min: {
+            minLength: {
               value: 7,
               message: `Min 7 numbers. Phone number must be digits and can contain spaces, dashes, parentheses and can start with +`,
             },
@@ -65,20 +77,19 @@ export const ContactForm = ({ onAddContact }) => {
               /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
           })}
         />
-        <div>
-          {errors?.number && (
-            <p>
-              {errors?.number?.message ||
-                `Phone number must be digits and can contain spaces, dashes, parentheses and can start with +`}
-            </p>
-          )}
-        </div>
-      </label>
+
+        {errors?.number && (
+          <ErrorMessage>
+            {errors?.number?.message ||
+              `Phone number must be digits and can contain spaces, dashes, parentheses and can start with +`}
+          </ErrorMessage>
+        )}
+      </FormField>
       <StyledButton type="submit" disabled={!isValid}>
         <IoMdPersonAdd size="16" />
         Add contact
       </StyledButton>
-    </form>
+    </Form>
   );
 };
 
